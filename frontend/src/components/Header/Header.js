cb0cde43608bb048baffa3386df9cd55d934bb6e
Michael Greatorex <magreatorex@gmail.com>
2023-09-14 19:31:08 +0100

    header progress

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,5 +1,39 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+import classes from './header.module.css'
 
 export default function Header() {
-    return <div>Header</div>;
+    const user = {
+        name: 'John',
+    };
+
+    const cart = {
+        totalCount: 10,
+    }
+
+    const logout = () => {}
+
+    return <header className={classes.header}>
+        <div className={classes.container}>
+            <Link to="/" className={classes.logo}>
+                Seeds of Destiny
+            </Link>
+            <nav>
+                <ul>
+                    {
+                        user?
+                        <li className={classes.menu_container}>
+                            <Link to="/profile">{user.name}</Link>
+                            <div className={classes.menu}>
+                                <Link to="/profile">Profile</Link>
+                                <Link to="/orders">Orders</Link>
+                                <a onClick={logout}>Logout</a>
+                            </div>
+                        </li> :
+                        <Link to="/login">Login</Link>
+                    }
+                </ul>
+            </nav>
+        </div>
+    </header>;
 }
